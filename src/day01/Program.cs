9c2697450b7cc94237f9dfdd12a1eb7a47946a38
lukaszadamus <lukasz.adamus@soodo.pl>
2022-12-01 10:14:14 +0100

    Solution

--- src/day01/Program.cs
+++ src/day01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt").Aggregate(new List<List<long>>() { new List<long>() }, (a, b) =>
+{
+    if (b.Trim().Length == 0)
+    {
+        a.Add(new());
+    }
+    else
+    {
+        a.Last().Add(long.Parse(b));
+    }
+    return a;
+}, c => c);
+
+var resultA = input.Select(x => x.Sum()).OrderByDescending(x => x).First();
+Console.WriteLine($"ResultA: {resultA}");
+
+var resultB = input.Select(x => x.Sum()).OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine($"ResultA: {resultB}");
