78b733bcedaabd430f8aa77956a6c5d323e9644c
lukaszadamus <lukasz.adamus@soodo.pl>
2022-12-06 09:20:38 +0100

    Day06

---
+++ src/day06/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllText("input.txt");
+Console.WriteLine($"ResultA: {Solve(input, 4)}");
+Console.WriteLine($"ResultB: {Solve(input, 14)}");
+
+int Solve(string input, int bufferLength)
+{
+    var processed = 0;
+    List<char> buffer = new();
+    foreach (var c in File.ReadAllText("input.txt"))
+    {
+        processed++;
+        buffer.Add(c);
+        if (buffer.Count == bufferLength)
+        {
+            if (!buffer.GroupBy(x => x).Select(x => x.Count()).Any(x => x > 1))
+            {
+                return processed;
+
+            }
+            buffer.RemoveAt(0);
+        }
+    }
+    return processed;
+}
