ac4510c251600e2ca9d7b5972f1d6c8dafb5c9cf
lukaszadamus <lukasz.adamus@soodo.pl>
2022-12-07 22:58:43 +0100

    Day07

---
+++ src/day07/Program.cs
@@ -0,0 +1,12 @@
+﻿var commands = System.IO.File.ReadAllLines("input.txt");
+
+var device = new Device(70000000);
+device.Boot(commands);
+
+var resultA = device.FlatFileSystem.Where(x => x.Value is Directory && x.Value.Size() < 100000).Select(x => x.Value.Size()).Sum();
+
+var freeUpSpace = 30000000 - device.UnusedSpace();
+var resultB = device.FlatFileSystem.Where(x => x.Value is Directory && x.Value.Size() >= freeUpSpace ).Select(x => x.Value.Size()).OrderBy(x => x).First();
+
+Console.WriteLine($"ResultA: {resultA}");
+Console.WriteLine($"ResultB: {resultB}");
