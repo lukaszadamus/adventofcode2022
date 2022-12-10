1a57cfc71602fd5f84ac80b1f935d9b85ddc9772
lukaszadamus <lukasz.adamus@soodo.pl>
2022-12-10 21:57:11 +0100

    :poop:

---
+++ src/day10/Program.cs
@@ -0,0 +1,18 @@
+﻿var cpu = new CPU();
+cpu.LoadInstructions("input.txt");
+var lcd = new LCD();
+
+Console.WriteLine($"ResultA: {SolveA(cpu)}");
+Console.WriteLine($"ResultB:");
+SolveB(cpu, lcd);
+
+int SolveA(CPU cpu) 
+    => cpu.GetSignalStrengthAt(20) 
+    + cpu.GetSignalStrengthAt(60) 
+    + cpu.GetSignalStrengthAt(100) 
+    + cpu.GetSignalStrengthAt(140) 
+    + cpu.GetSignalStrengthAt(180) 
+    + cpu.GetSignalStrengthAt(220);
+
+void SolveB(CPU cpu, LCD lcd)
+    => cpu.DisplayOnLcd(lcd);
